dc4063ad56df6db489ae58e9f07e611fe90bf379
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-26 10:41:09 -0300

    Sellers Controller

---
+++ ProjetoWebPageMVC/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoWebPageMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
