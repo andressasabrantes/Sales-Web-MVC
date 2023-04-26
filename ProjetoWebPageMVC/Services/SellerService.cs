89a00d166188096bddaa6f7d4cc4bbeabbabd0d7
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-26 16:00:12 -0300

    Foreign key not null (referential integrity)

--- ProjetoWebPageMVC/Services/SellerService.cs
+++ ProjetoWebPageMVC/Services/SellerService.cs
@@ -19,6 +19,7 @@ namespace ProjetoWebPageMVC.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
