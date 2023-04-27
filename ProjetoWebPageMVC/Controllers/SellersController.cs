24374800b0e4c6c27997c852f13cedcd06cfc9bd
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-27 11:39:25 -0300

    Returning custom error page

--- ProjetoWebPageMVC/Controllers/SellersController.cs
+++ ProjetoWebPageMVC/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using ProjetoWebPageMVC.Models;
 using ProjetoWebPageMVC.Models.ViewModels;
 using ProjetoWebPageMVC.Services;
 using ProjetoWebPageMVC.Services.Exceptions;
+using System.Diagnostics;
 
 namespace ProjetoWebPageMVC.Controllers
 {
@@ -41,13 +42,13 @@ namespace ProjetoWebPageMVC.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = "Id not provided"});
             }
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
@@ -65,13 +66,13 @@ namespace ProjetoWebPageMVC.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
@@ -81,13 +82,13 @@ namespace ProjetoWebPageMVC.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             List<Department> departments = _departmentService.FindAll();
@@ -101,21 +102,27 @@ namespace ProjetoWebPageMVC.Controllers
         {
             if (id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
             try
             {
                 _sellerService.Update(seller);
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch (ApplicationException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message});
             }
-            catch (DbConcurrencyException)
+        }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
             {
-                return BadRequest();
-            }
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
