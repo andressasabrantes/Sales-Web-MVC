2adc615fefb978c5fa2d3618d2d6f5eb5c660109
Andressa Abrantes <andressaabrantes@gmail.com>
2023-04-27 09:51:53 -0300

    seller details and eager loading

--- ProjetoWebPageMVC/Services/SellerService.cs
+++ ProjetoWebPageMVC/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using ProjetoWebPageMVC.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoWebPageMVC.Data;
 using ProjetoWebPageMVC.Models;
 
 namespace ProjetoWebPageMVC.Services
@@ -25,7 +26,7 @@ namespace ProjetoWebPageMVC.Services
 
         public Seller FindById (int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
